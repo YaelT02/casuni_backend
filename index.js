94e74400e70972869d965b034be0084f6e9a1e66
Yaelt02 <sergioyaeltrejomaya@gmail.com>
2025-04-09 16:56:04 -0600

    Cursos

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes'); // Importar las rutas de autenticación
-const manualsRoutes = require('./routes/manualsRoutes')
-const logsRoutes = require('./routes/logsRoutes')
+const manualsRoutes = require('./routes/manualsRoutes');
+const courseRoutes = require('./routes/courseRoutes');         
+const moduleRoutes = require('./routes/moduleRoutes');         
+const contentRoutes = require('./routes/contentRoutes');       
+const enrollmentRoutes = require('./routes/enrollmentRoutes'); 
+const attemptRoutes = require('./routes/attemptRoutes');   
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +22,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/manuals', manualsRoutes);
 app.use('/api/logs', logsRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/manuals', manualsRoutes);
+app.use('/api/logs', logsRoutes);
+app.use('/api/courses', courseRoutes);
+app.use('/api/modules', moduleRoutes);
+app.use('/api/contents', contentRoutes);
+app.use('/api/enrollments', enrollmentRoutes);
+app.use('/api/attempts', attemptRoutes);
 
 // Iniciar Servidor
 app.listen(PORT, () => {
